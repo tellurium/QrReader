f8c0ddf8ce09a4866d750cdbaba9a3e59d111dfa
tellurium <huairenpp@gmail.com>
2012-03-04 00:25:33 +0800

    normal...

--- src/cn/edu/shu/apps/QrReader/MainActivity.java
+++ src/cn/edu/shu/apps/QrReader/MainActivity.java
@@ -3,12 +3,10 @@ package cn.edu.shu.apps.QrReader;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity
-{
-    /** Called when the activity is first created. */
+public class MainActivity extends Activity {
+
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
